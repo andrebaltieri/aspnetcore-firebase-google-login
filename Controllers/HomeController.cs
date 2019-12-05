4ed46dc266c21228172ed1387b4a9870b3516f50
Andre Baltieri <andrebaltieri@hotmail.com>
2019-12-04 23:06:22 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fire.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public dynamic Get()
+        {
+            return new
+            {
+                name = User.Claims.Where(x => x.Type == "name").FirstOrDefault().Value,
+                // email = User.Claims.Where(x => x.Type == "email").FirstOrDefault().Value,
+                picture = User.Claims.Where(x => x.Type == "picture").FirstOrDefault().Value,
+            };
+        }
+    }
+}
